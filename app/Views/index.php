540a4ea378a28b7a78e8aa06d3f36369f4c2709e
Mathys <poretmathys2004@gmail.com>
2024-12-02 15:02:13 +0100

    Implémentation page accueil

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,12 +1,12 @@
 <?php 
     $title = "Saiyan's Coaching - Accueil"; 
-    include 'templates/header.php'; 
+    include 'templates/header.php';
 ?>
-
+<?php include 'templates/navbar.php'; ?>
 <?php include 'templates/accueil.php'; ?>
 
-<div id="titreOffre">
-    <h1>NOS OFFRES :</h1>
+<div class="tJ" id="titreOffre">
+    <h2>NOS OFFRES :</h2>
 </div>
 
 <?php include 'templates/footer.php'; ?>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -13,18 +13,3 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 </head>
 <body>
-    <nav>
-		<div class="left-section">
-			<img src="/assets/images/logo.png" alt="Logo">
-		</div>
-        <ul>
-            <li><a href="/">Accueil</a></li>
-            <li><a href="/about">À propos</a></li>
-			<li><a href="/about">Programmes</a></li>
-            <li><a href="/contact">Avant / Après</a></li>
-			<li><a href="/about">Blog</a></li>
-            <li><a href="/contact">Actualité</a></li>
-            <li><a href="/contact">Contact</a></li>
-			<li><button class="btnFGBJ">Se Connecter</button></li>
-        </ul>
-    </nav>
