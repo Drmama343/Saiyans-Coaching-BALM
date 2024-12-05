f6ad7f81caac3144b53947493b593342706209c2
Frizoks <luc.lecarpentier5@gmail.com>
2024-12-05 09:27:19 +0100

    avis

--- app/Views/templates/temoignages.php
+++ app/Views/templates/temoignages.php
@@ -1,21 +1,20 @@
 <div class="conteneur-temoignage">
-    <h2>Témoignages</h2>
-    <div class="carousel">
-        <div class="contenu-temoignage">
-			<?php if (!empty($achats) && is_array($achats)): ?>
-				<?php foreach ($achats as $achat): ?>
+	<h2>Témoignages</h2>
+	<div class="carousel">
+		<div class="contenu-temoignage">
+			<?php if (!empty($avis) && is_array($avis)): ?>
+				<?php foreach ($avis as $avi): ?>
 					<div class="temoignage">
 						<span class="tJ"><span class="etoile"></span><span class="etoile"></span><span class="etoile"></span><span class="etoile"></span><span class="etoile"></span></span>
-						<p><?= esc($achat['temoignage']) ?></p>
-						<h4><?= esc($achat['idsaiyan']) ?></h4>
+						<p><?= esc($avi['temoignage']) ?></p>
+						<h4><?= esc($avi['idsaiyan']) ?></h4> 
 					</div>
 				<?php endforeach; ?>
 				<?php else: ?>
-				<p>Aucune temoignange trouvée.</p>
+				<p>Aucun temoignange trouvée.</p>
 			<?php endif; ?>
-			</div>
-        </div>
-        <button class="prev">◀</button>
-        <button class="next">▶</button>
-    </div>
+		</div>
+		<button class="prev">◀</button>
+		<button class="next">▶</button>
+	</div>
 </div>
