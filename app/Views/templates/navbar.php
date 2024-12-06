2fdc0041c6c48bb7d4ff8423bf6e68bc86e52f7b
Frizoks <luc.lecarpentier5@gmail.com>
2024-12-06 11:21:21 +0100

    modal compte

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -17,7 +17,7 @@
 		<li><a href="/actualite">Actualité</a></li>
 		<li><a href="/contact">Contact</a></li>
 		<?php if (isset($_SESSION['utilisateur'])): ?>
-		<li><img id="openModalCompte" class="imgProfil" src="/assets/images/profil.png" alt="Image profil"></li>
+		<li><button class="btnModal" id="openModalCompte"><img class="imgProfil" src="/assets/images/profil.png" alt="Image profil"></button></li>
 		<?php else : ?>
 		<li><a href="/connexion" class="btnFGBJ">Se Connecter</a></li>
 		<?php endif; ?>
@@ -25,11 +25,11 @@
 </nav>
 
 <!-- Modal -->
-<div id="compteModal" class="modal">
+<div id="creationCompteModal" class="modal">
 	<div class="modal-content">
 		<span class="close-btn" id="closeModalCompte">&times;</span>
 		<button id="compteBtn"><h5>Mon compte</h5></button>
-		<button id="adminBtn"><h5>Admin</h5></button>
+		<?php if (isset($_SESSION['utilisateur'])): ?><button id="adminBtn"><h5>Admin</h5></button><?php endif; ?>
 		<button id="logoutBtn"><h5>Déconnexion</h5></button>
 	</div>
 </div>
--- public/assets/js/fctNavbar.js
+++ public/assets/js/fctNavbar.js
@@ -2,3 +2,23 @@ function toggleMenu() {
 	const menu = document.querySelector('nav ul');
 	menu.classList.toggle('active');
 }
+
+const modalCompte = document.getElementById('creationCompteModal');
+const openModalCompte = document.getElementById('openModalCompte');
+const closeModalCompte = document.getElementById('closeModalCompte');
+
+// Ouvrir le modal
+openModalCompte.addEventListener('click', () => {
+	modalCompte.style.display = 'flex';
+});
+
+closeModalCompte.addEventListener('click', () => {
+	modalCompte.style.display = 'none';
+});
+
+// Fermer le modal si l'utilisateur clique en dehors du modal
+window.addEventListener('click', (event) => {
+	if (event.target === modalCompte) {
+		modalCompte.style.display = 'none';
+	}
+});
