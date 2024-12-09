2b85184ff3e6768a9746338f0a8c11069a282d71
Frizoks <luc.lecarpentier5@gmail.com>
2024-12-09 09:40:10 +0100

    statistiques

--- app/Views/admin/admin.php
+++ app/Views/admin/admin.php
@@ -1,11 +1,11 @@
 <?php
 	$title = "Saiyan's Coaching - Admin";
-	$style = "stlAccueil.css";
+	$style = "stlStats.css";
 	$navbar = "stlNavbar.css";
 	include __DIR__ . '/../templates/header.php';
 	include __DIR__ . '/../templates/navbarAdmin.php';
 ?>
 
-
+<?php include __DIR__ . '/../admin/stats.php'; ?>
 
 <?php include __DIR__ . '/../templates/footer.php'; ?>
